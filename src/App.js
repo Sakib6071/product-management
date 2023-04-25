dcf155509cdbeb99fcba4e5f0a8e1cc691ebedbb
Your Name <you@example.com>
2023-04-25 16:13:22 +0600

    Product Management project initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Components/Home/Home';
+import AddProduct from './Components/AddProduct/AddProduct';
+import Manage from './Components/Manage/Manage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/add-product' element={<AddProduct></AddProduct>}></Route>
+        <Route path='/manage' element={<Manage></Manage>}></Route>
+      </Routes>
+      
     </div>
   );
 }
